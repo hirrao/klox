8c032589d7e0129566fa8f54362b143101c53077
hirrao <git@hirrao.com>
2026-01-06 01:04:00 +0800

    Representing Code

---
+++ src/main/kotlin/com/hirrao/klox/ast/AstPrinter.kt
@@ -0,0 +1,34 @@
+package com.hirrao.klox.ast
+
+import com.hirrao.klox.token.Token
+import com.hirrao.klox.token.TokenType
+
+object AstPrinter {
+
+    fun printAst(expr: Expr): String = when (expr) {
+        is Expr.Binary -> parenthesize(expr.operator.lexeme, expr.left, expr.right)
+        is Expr.Call -> TODO()
+        is Expr.Get -> TODO()
+        is Expr.Grouping -> parenthesize("Group", expr.expression)
+        is Expr.Literal -> expr.value?.toString() ?: "nil"
+        is Expr.Logical -> TODO()
+        is Expr.Set -> TODO()
+        is Expr.Super -> TODO()
+        is Expr.This -> TODO()
+        is Expr.Unary -> parenthesize(expr.operator.lexeme, expr.right)
+        is Expr.Variable -> TODO()
+    }
+
+    private fun parenthesize(name: String, vararg exprs: Expr): String {
+        return "($name ${exprs.map(::printAst).reduce { acc, expr -> "$acc $expr" }})"
+    }
+}
+
+fun main() {
+    val expression = Expr.Binary(
+        Expr.Unary(Token(TokenType.MINUS, "-", null, 1), Expr.Literal(123)),
+        Token(TokenType.STAR, "*", null, 1),
+        Expr.Grouping(Expr.Literal(45.67))
+    )
+    println(AstPrinter.printAst(expression))
+}
---
+++ src/main/kotlin/com/hirrao/klox/ast/Expr.kt
@@ -0,0 +1,17 @@
+package com.hirrao.klox.ast
+
+import com.hirrao.klox.token.Token
+
+sealed class Expr {
+    class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr()
+    class Call(val callee: Expr, val paran: Token, val argument: List<Expr>) : Expr()
+    class Get(val obj: Expr, val name: Token) : Expr()
+    class Grouping(val expression: Expr) : Expr()
+    class Literal(val value: Any?) : Expr()
+    class Logical(val left: Expr, val operator: Token, val right: Expr) : Expr()
+    class Set(val obj: Expr, val name: Token, val value: Expr) : Expr()
+    class Super(val keyword: Token, val method: Token) : Expr()
+    class This(val keyword: Token) : Expr()
+    class Unary(val operator: Token, val right: Expr) : Expr()
+    class Variable(val name: Token) : Expr()
+}
